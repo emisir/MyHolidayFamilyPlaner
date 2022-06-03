8e3160645b2a09f47b3f1ebbdbd41de864ac6dc6
Burak <brk_seyrek@outlook.com>
2022-06-04 01:59:03 +0200

    Ziele, Familienmitglieder hinzufügen

---
+++ public/js/dialoge.js
@@ -0,0 +1,21 @@
+function dialogOeffnen(dialogId) {
+  document.getElementById(dialogId).classList.add("sichtbar");
+  document.getElementById("body-overlay").classList.add("sichtbar");
+}
+
+
+function dialogSchliessen(dialogId) {
+  document.getElementById(dialogId).classList.remove("sichtbar")
+  document.getElementById("body-overlay").classList.remove("sichtbar");
+
+}
+
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
