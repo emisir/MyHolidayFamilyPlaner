ac1be7c7233abdb347ca7e54feecdc29588142fc
aylinyamac <aylin.yamac01@studmail.htw-aalen.de>
2022-06-07 14:43:54 +0200

    HolidaywishController,-Service und Repository Klassen verbessert

--- src/main/java/hs/aalen/Holidaywish/HolidaywishService.java
+++ src/main/java/hs/aalen/Holidaywish/HolidaywishService.java
@@ -1,44 +1,42 @@
 package hs.aalen.Holidaywish;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
-
-
 public class HolidaywishService {
+@Autowired
+    private HolidaywishRepository holidaywishrepository;
 
-	private HolidaywishRepository holidaywishrepository;
-	
-	 public List<Holidaywish> getHolidaywishList() {
-	        ArrayList<Holidaywish> mylist1 = new ArrayList<>();
-	        Iterator<Holidaywish> it = holidaywishrepository.findAll().iterator();
-	        while (it.hasNext())
-	            mylist1.add(it.next());
-	        return mylist1;
+     public List<Holidaywish> getHolidaywishList() {
+            ArrayList<Holidaywish> mylist = new ArrayList<>();
+            Iterator<Holidaywish> it = holidaywishrepository.findAll().iterator();
+            while (it.hasNext())
+                mylist.add(it.next());
+            return mylist;
 
-	 }
+     }
 
-	public Holidaywish getHolidaywishList(String description) {
-		return holidaywishrepository.findById(description).orElse(null);
-	}
+    public Holidaywish getHolidaywish(String description) {
+        return holidaywishrepository.findById(description).orElse(null);
+    }
 
-	public void addHolidaywish(Holidaywish holidaywish) {
-		holidaywishrepository.save(holidaywish);
-		
-	}
+    public void addHolidaywish(Holidaywish holidaywish) {
+        holidaywishrepository.save(holidaywish);
 
-	public void updateHolidaywish(String description, Holidaywish holidaywish) {
-		holidaywishrepository.save(holidaywish);
-		
-	}
+    }
 
-	public void deleteHolidaywish(String description, Holidaywish holidaywish) {
-		holidaywishrepository.deleteById(description);
-		
-	}
+    public void updateHolidaywish(String description, Holidaywish holidaywish) {
+        holidaywishrepository.save(holidaywish);
+
+    }
+
+    public void deleteHolidaywish(String description) {
+        holidaywishrepository.deleteById(description);
+
+    }
+
 
 }
