ce9fcd328a3282d66064a44b4cfb6a1425de7b20
aylinyamac <aylin.yamac01@studmail.htw-aalen.de>
2022-06-07 14:21:46 +0200

    FamilymemberController,-Service und -Repository Klassen verbessert

--- src/main/java/hs/aalen/Familymember/Familymember.java
+++ src/main/java/hs/aalen/Familymember/Familymember.java
@@ -1,70 +1,52 @@
 package hs.aalen.Familymember;
 
-import java.util.Date;
-import java.util.List;
-
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import hs.aalen.Holidaywish.Holidaywish;
-
-
 @Entity
-
-
 public class Familymember {
-	@Id
-	private String name;
-	private long id;
-	private Date bday;
-	
-//beziehung m to n zu Holidaywish
-	@ManyToMany(mappedBy="prio")
-	private List<Holidaywish> holidaywishprio;
-		
-	public Familymember() {
-	}
-	public Familymember(String name, long id, Date bday) {
-		super();
-		this.name = name;
-		this.id = id;
-		this.bday = bday;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public Date getBday() {
-		return bday;
-	}
-
-	public void setBday(Date bday) {
-		this.bday = bday;
-
-	}
-	public List<Holidaywish> setHolidaywishprio(){
-		return holidaywishprio ;
-		
-	}
-	 public void getHolidaywishprio(List<Holidaywish> holidaywishprio) {
-		 this.holidaywishprio = holidaywishprio ;
-	 }
-
-}	
+@Id
+    private String name;
+    private String id;
+    private String bday;
 
 
 
+    public Familymember() {
+    }
 
-	
-	
+    public Familymember(String name, String id, String bday) {
+        super();
+        this.name = name;
+        this.id = id;
+        this.bday = bday;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public void setBday(String bday) {
+        this.bday = bday;
+
+
+    }
+
+}
--- src/main/java/hs/aalen/Familymember/FamilymemberRepository.java
+++ src/main/java/hs/aalen/Familymember/FamilymemberRepository.java
@@ -1,5 +1,7 @@
 package hs.aalen.Familymember;
-import org.springframework.data.repository.CrudRepository;
-public interface FamilymemberRepository extends CrudRepository<Familymember, String> {
-}
 
+import org.springframework.data.repository.CrudRepository;
+
+public interface FamilymemberRepository extends CrudRepository<Familymember, String> {
+
+}
