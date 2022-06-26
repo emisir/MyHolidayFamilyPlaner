15a57c7e866612826bc2298d6205c9b2e4a62ef5
Atilla Durdu <atilla.durdu@studmail.htw-aalen.de>
2022-06-26 16:57:20 +0200

    holidayWishService für Controller angepasst und Repository <list> hinzugefügt

--- src/main/java/hs/aalen/Holidaywish/HolidayWishRepository.java
+++ src/main/java/hs/aalen/Holidaywish/HolidayWishRepository.java
@@ -1,7 +1,11 @@
 package hs.aalen.Holidaywish;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface HolidayWishRepository extends CrudRepository<HolidayWish, Long> {
+
+    List<HolidayWish> findAll();
 
 }
--- src/main/java/hs/aalen/Holidaywish/HolidayWishService.java
+++ src/main/java/hs/aalen/Holidaywish/HolidayWishService.java
@@ -1,19 +1,44 @@
 package hs.aalen.Holidaywish;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HolidayWishService {
-	@Autowired
-	private HolidayWishRepository holidayWishRepository;
+    @Autowired
+    private HolidayWishRepository holidayWishRepository;
 
-	public HolidayWishRepository getHolidayWishRepository() {
-		return holidayWishRepository;
-	}
+    public HolidayWishRepository getHolidayWishRepository() {
+        return holidayWishRepository;
+    }
 
-	public void setHolidayWishRepository(HolidayWishRepository holidayWishRepository) {
-		this.holidayWishRepository = holidayWishRepository;
-	}
+    public void setHolidayWishRepository(HolidayWishRepository holidayWishRepository) {
+        this.holidayWishRepository = holidayWishRepository;
+    }
+
+    public List<HolidayWish> getHolidayWishList() {
+        return holidayWishRepository.findAll();
+    }
+ 
+
+    public HolidayWish getHolidayWish(Long id) {
+        return holidayWishRepository.findById(id).orElse(null);
+    }
+
+    public void addHolidayWish(HolidayWish holidayWish) {
+        holidayWishRepository.save(holidayWish);
+    }
+
+    public void updateHolidayWish(Long id, HolidayWish holidayWish) {
+        holidayWishRepository.save(holidayWish);
+    }
+
+    public void deleteHolidayWish(Long id) {
+        holidayWishRepository.deleteById(id);
+    }
+
+
 
 }
