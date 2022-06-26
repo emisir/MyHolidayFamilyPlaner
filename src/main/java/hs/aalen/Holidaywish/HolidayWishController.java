e1f7a6544bc76900fa60394b1a1d1d1d3f3f5cff
Atilla Durdu <atilla.durdu@studmail.htw-aalen.de>
2022-06-26 16:12:01 +0200

    HolidayWishController entkommentiert und angepasst/verbessert

--- src/main/java/hs/aalen/Holidaywish/HolidayWishController.java
+++ src/main/java/hs/aalen/Holidaywish/HolidayWishController.java
@@ -1,38 +1,45 @@
 package hs.aalen.Holidaywish;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class HolidayWishController {
 
-//	@Autowired
-//	HolidayWishService holidaywishservice;
-//
-//	@RequestMapping("/holiday-wish")
-//	public List<HolidayWish> getHolidaywishList() {
-//		return holidaywishservice.getHolidayWishList();
-//	}
-//
-//	@RequestMapping("/holiday-wish/{description}")
-//	public HolidayWish getHolidaywish(@PathVariable Long id) {
-//		return holidaywishservice.getHolidayWish(id);
-//
-//	}
-//
-//	@RequestMapping(method = RequestMethod.POST, value = "/holiday-wish")
-//	public void addHolidaywish(@RequestBody HolidayWish holidaywish) {
-//		holidaywishservice.addHolidayWish(holidaywish);
-//
-//	}
-//
-//	@RequestMapping(method = RequestMethod.PUT, value = "/holiday-wish/{description}")
-//	public void updateHolidaywish(@PathVariable String description, @RequestBody HolidayWish holidaywish) {
-//		holidaywishservice.updateHolidayWish(description, holidaywish);
-//	}
-//
-//	@RequestMapping(method = RequestMethod.DELETE, value = "/holiday-wish/{description}")
-//	public void deleteHolidaywish(@PathVariable String description, @RequestBody HolidayWish holidaywish) {
-//		holidaywishservice.deleteHolidayWish(description);
-//	}
+    @Autowired
+    HolidayWishService holidaywishservice;
+
+    @RequestMapping("/holiday-wish")
+    public List<HolidayWish> getHolidaywishList() {
+        return holidaywishservice.getHolidayWishList();
+    }
+
+    @RequestMapping("/holiday-wish/{id}")
+    public HolidayWish getHolidaywish(@PathVariable Long id) {
+        return holidaywishservice.getHolidayWish(id);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/holiday-wish")
+    public void addHolidaywish(@RequestBody HolidayWish holidayWish) {
+        holidaywishservice.addHolidayWish(holidayWish);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/holiday-wish/{id}")
+    public void updateHolidaywish(@PathVariable Long id, @RequestBody HolidayWish holidayWish) {
+        holidaywishservice.updateHolidayWish(id, holidayWish);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/holiday-wish/{id}")
+    public void deleteHolidaywish(@PathVariable Long id) {
+        holidaywishservice.deleteHolidayWish(id);
+    }
 }
